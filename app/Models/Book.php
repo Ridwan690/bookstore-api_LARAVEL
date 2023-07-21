107df32b818dbe11175d5ab2d86c9fb7363c1e4e
Ridwan Nurhakim <ridwann690@gmail.com>
2023-07-22 00:34:11 +0700

    up

---
+++ app/Models/Book.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Book extends Model
+{
+    protected $fillable = [
+        'title', 'slug', 'description', 'author', 'publisher', 'cover', 'price', 'weight', 'stock', 'status'
+    ];
+
+    public function categories() {
+        return $this->belongsToMany('App\Models\Category');
+    }
+}
